be4aff7d17bb131b537ef019530eabadcd777a10
Marco Chan <inspiredtolive0@gmail.com>
2016-09-14 16:37:21 -0700

    Set up watson tone analysis api endpoint

---
+++ keys.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  facebook: {
+    FACEBOOK_APP_ID: 'FACEBOOK_APP_ID',
+    FACEBOOK_APP_SECRET: 'FACEBOOK_APP_SECRET'
+  },
+  aylien: {
+    app_id: 'app_id',
+    app_key: 'app_key'
+  },
+  watson: {
+    user: 'service_username', 
+    pass: 'service_password'
+  }
+};
